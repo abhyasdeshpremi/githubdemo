96e3bfa8407e45be54448b67c931ee5da897f860
Techment DEV <techmentdev@techments-mac-mini-2.local>
2016-02-18 17:19:29 +0530

    Initial Commit

---
+++ github demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  github demo
+//
+//  Created by Techment DEV on 2/18/16.
+//  Copyright © 2016 Techment DEV. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ github demo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  github demo
+//
+//  Created by Techment DEV on 2/18/16.
+//  Copyright © 2016 Techment DEV. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
